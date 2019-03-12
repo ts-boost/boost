e80b1f00896a493164a7e39ffe641ac5521f42bd
Frederik Krautwald <fkrautwald@gmail.com>
2019-03-13 00:40:37 +0100

    Remove unpublished core package
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/core/src/functional.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2017 Frederik Krautwald.
-// Distributed under the TS-Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.md or copy at
-// https://github.com/ts-boost/boost/wiki/License)
-
-export * from '167'
-export { map as rMap } from '167'
--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2017 Frederik Krautwald.
-// Distributed under the TS-Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.md or copy at
-// https://github.com/ts-boost/boost/wiki/License)
-
-export * from './functional'
