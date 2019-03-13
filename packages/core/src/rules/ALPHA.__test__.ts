687018d8fb67a15a06525b5f69fc842f84df7364
Frederik Krautwald <fkrautwald@gmail.com>
2019-03-13 05:12:59 +0100

    Add `ALPHA` type
    This commit adds an `ALPHA` type, i.e., A-Z / a-z.
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

---
+++ packages/core/src/rules/ALPHA.__test__.ts
@@ -0,0 +1,213 @@
+import { describe, it, Test } from '@typed/test'
+import { ALPHA } from './ALPHA'
+
+export const test: Test = describe('ALPHA', [
+    it('can be "A"', ({ equal }) => {
+        const a: ALPHA = 'A'
+        equal(a, a)
+    }),
+    it('can be "B"', ({ equal }) => {
+        const a: ALPHA = 'B'
+        equal(a, a)
+    }),
+    it('can be "C"', ({ equal }) => {
+        const a: ALPHA = 'C'
+        equal(a, a)
+    }),
+    it('can be "D"', ({ equal }) => {
+        const a: ALPHA = 'D'
+        equal(a, a)
+    }),
+    it('can be "E"', ({ equal }) => {
+        const a: ALPHA = 'E'
+        equal(a, a)
+    }),
+    it('can be "F"', ({ equal }) => {
+        const a: ALPHA = 'F'
+        equal(a, a)
+    }),
+    it('can be "G"', ({ equal }) => {
+        const a: ALPHA = 'G'
+        equal(a, a)
+    }),
+    it('can be "H"', ({ equal }) => {
+        const a: ALPHA = 'H'
+        equal(a, a)
+    }),
+    it('can be "I"', ({ equal }) => {
+        const a: ALPHA = 'I'
+        equal(a, a)
+    }),
+    it('can be "J"', ({ equal }) => {
+        const a: ALPHA = 'J'
+        equal(a, a)
+    }),
+    it('can be "K"', ({ equal }) => {
+        const a: ALPHA = 'K'
+        equal(a, a)
+    }),
+    it('can be "L"', ({ equal }) => {
+        const a: ALPHA = 'L'
+        equal(a, a)
+    }),
+    it('can be "M"', ({ equal }) => {
+        const a: ALPHA = 'M'
+        equal(a, a)
+    }),
+    it('can be "N"', ({ equal }) => {
+        const a: ALPHA = 'N'
+        equal(a, a)
+    }),
+    it('can be "O"', ({ equal }) => {
+        const a: ALPHA = 'O'
+        equal(a, a)
+    }),
+    it('can be "P"', ({ equal }) => {
+        const a: ALPHA = 'P'
+        equal(a, a)
+    }),
+    it('can be "Q"', ({ equal }) => {
+        const a: ALPHA = 'Q'
+        equal(a, a)
+    }),
+    it('can be "R"', ({ equal }) => {
+        const a: ALPHA = 'R'
+        equal(a, a)
+    }),
+    it('can be "S"', ({ equal }) => {
+        const a: ALPHA = 'S'
+        equal(a, a)
+    }),
+    it('can be "T"', ({ equal }) => {
+        const a: ALPHA = 'T'
+        equal(a, a)
+    }),
+    it('can be "U"', ({ equal }) => {
+        const a: ALPHA = 'U'
+        equal(a, a)
+    }),
+    it('can be "V"', ({ equal }) => {
+        const a: ALPHA = 'V'
+        equal(a, a)
+    }),
+    it('can be "W"', ({ equal }) => {
+        const a: ALPHA = 'W'
+        equal(a, a)
+    }),
+    it('can be "X"', ({ equal }) => {
+        const a: ALPHA = 'X'
+        equal(a, a)
+    }),
+    it('can be "Y"', ({ equal }) => {
+        const a: ALPHA = 'Y'
+        equal(a, a)
+    }),
+    it('can be "Z"', ({ equal }) => {
+        const a: ALPHA = 'Z'
+        equal(a, a)
+    }),
+    it('can be "a"', ({ equal }) => {
+        const a: ALPHA = 'a'
+        equal(a, a)
+    }),
+    it('can be "b"', ({ equal }) => {
+        const a: ALPHA = 'b'
+        equal(a, a)
+    }),
+    it('can be "c"', ({ equal }) => {
+        const a: ALPHA = 'c'
+        equal(a, a)
+    }),
+    it('can be "d"', ({ equal }) => {
+        const a: ALPHA = 'd'
+        equal(a, a)
+    }),
+    it('can be "e"', ({ equal }) => {
+        const a: ALPHA = 'e'
+        equal(a, a)
+    }),
+    it('can be "f"', ({ equal }) => {
+        const a: ALPHA = 'f'
+        equal(a, a)
+    }),
+    it('can be "g"', ({ equal }) => {
+        const a: ALPHA = 'g'
+        equal(a, a)
+    }),
+    it('can be "h"', ({ equal }) => {
+        const a: ALPHA = 'h'
+        equal(a, a)
+    }),
+    it('can be "i"', ({ equal }) => {
+        const a: ALPHA = 'i'
+        equal(a, a)
+    }),
+    it('can be "j"', ({ equal }) => {
+        const a: ALPHA = 'j'
+        equal(a, a)
+    }),
+    it('can be "k"', ({ equal }) => {
+        const a: ALPHA = 'k'
+        equal(a, a)
+    }),
+    it('can be "l"', ({ equal }) => {
+        const a: ALPHA = 'l'
+        equal(a, a)
+    }),
+    it('can be "m"', ({ equal }) => {
+        const a: ALPHA = 'm'
+        equal(a, a)
+    }),
+    it('can be "n"', ({ equal }) => {
+        const a: ALPHA = 'n'
+        equal(a, a)
+    }),
+    it('can be "o"', ({ equal }) => {
+        const a: ALPHA = 'o'
+        equal(a, a)
+    }),
+    it('can be "p"', ({ equal }) => {
+        const a: ALPHA = 'p'
+        equal(a, a)
+    }),
+    it('can be "q"', ({ equal }) => {
+        const a: ALPHA = 'q'
+        equal(a, a)
+    }),
+    it('can be "r"', ({ equal }) => {
+        const a: ALPHA = 'r'
+        equal(a, a)
+    }),
+    it('can be "s"', ({ equal }) => {
+        const a: ALPHA = 's'
+        equal(a, a)
+    }),
+    it('can be "t"', ({ equal }) => {
+        const a: ALPHA = 't'
+        equal(a, a)
+    }),
+    it('can be "u"', ({ equal }) => {
+        const a: ALPHA = 'u'
+        equal(a, a)
+    }),
+    it('can be "v"', ({ equal }) => {
+        const a: ALPHA = 'v'
+        equal(a, a)
+    }),
+    it('can be "w"', ({ equal }) => {
+        const a: ALPHA = 'w'
+        equal(a, a)
+    }),
+    it('can be "x"', ({ equal }) => {
+        const a: ALPHA = 'x'
+        equal(a, a)
+    }),
+    it('can be "y"', ({ equal }) => {
+        const a: ALPHA = 'y'
+        equal(a, a)
+    }),
+    it('can be "z"', ({ equal }) => {
+        const a: ALPHA = 'z'
+        equal(a, a)
+    }),
+])
